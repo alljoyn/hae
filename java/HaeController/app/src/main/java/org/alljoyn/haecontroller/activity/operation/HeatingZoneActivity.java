9f9fef79ee03230b7351792f81006d6aa555b0be
Liubov76 <liubov.danilovtseva@electrolux.com>
2016-05-30 13:33:45 +0300

    Added some activities to the HAE Android Controller
    Change-Id: Ie5e318994b30bc874380cbefe8a344ac836da64f
    Signed-off-by: Liubov76 <liubov.danilovtseva@electrolux.com>

--- java/HaeController/app/src/main/java/org/alljoyn/haecontroller/activity/operation/HeatingZoneActivity.java
+++ java/HaeController/app/src/main/java/org/alljoyn/haecontroller/activity/operation/HeatingZoneActivity.java
@@ -19,18 +19,12 @@ package org.alljoyn.haecontroller.activity.operation;
 import android.view.View;
 
 import org.alljoyn.haecontroller.activity.InterfaceActivity;
-import org.alljoyn.haecontroller.view.method.MethodView;
-import org.alljoyn.haecontroller.view.property.SupportedVendorValuesAndEnumPropertyView;
-import org.alljoyn.smartspaces.operation.DishWashingCyclePhase;
+import org.alljoyn.haecontroller.view.property.ListWithSubValuesPropertyView;
 
-public class CyclePhaseActivity extends InterfaceActivity {
+public class HeatingZoneActivity extends InterfaceActivity {
     @Override
-    protected void generatePropertyView(InterfaceActivity.CustomView properties, CustomView methods) {
-
-        View cycleView = new SupportedVendorValuesAndEnumPropertyView(this, this.intf, "CyclePhase", null, 1, 0, "SupportedCyclePhases", DishWashingCyclePhase.CyclePhase.class, "GetVendorPhasesDescription", "en");
-        properties.addView(cycleView);
-
-        View getVendorPhasesDescriptionView = new MethodView(this, this.intf, "GetVendorPhasesDescription", "languageTag");
-        methods.addView(getVendorPhasesDescriptionView);
+    protected void generatePropertyView(CustomView properties, CustomView methods) {
+        View zonesView = new ListWithSubValuesPropertyView(this, this.intf, "NumberOfHeatingZones", "HeatingZone", null, "MaxHeatingLevels", "HeatingLevels");
+        properties.addView(zonesView);
     }
 }
